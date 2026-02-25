93d3e8a5377f4083db4af91f1b5f3aeb9463e9c8
JuanPFrancoL <juanp.franco@autonoma.edu.co>
2026-02-25 17:02:08 -0500

    Creacion de Botones

--- Ejercicio Hospital/src/HospitalGUI.java
+++ Ejercicio Hospital/src/HospitalGUI.java
@@ -1,2 +1,11 @@
+import javax.swing.*;
+
 public class HospitalGUI {
+    private JPanel SDAW;
+    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPane2;
+    private JTabbedPane tabbedPane3;
+    private JTabbedPane tabbedPane4;
+
+
 }
