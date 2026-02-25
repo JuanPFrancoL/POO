ec268a410a13435e7cd80fd3b265d4dd74d4ecef
JuanPFrancoL <juanp.franco@autonoma.edu.co>
2026-02-25 17:15:39 -0500

    Creacion ventana Medicos

--- Ejercicio Hospital/src/HospitalGUI.java
+++ Ejercicio Hospital/src/HospitalGUI.java
@@ -3,9 +3,11 @@ import javax.swing.*;
 public class HospitalGUI {
     private JPanel SDAW;
     private JTabbedPane tabbedPane1;
-    private JTabbedPane tabbedPane2;
-    private JTabbedPane tabbedPane3;
-    private JTabbedPane tabbedPane4;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
 
 
 }
